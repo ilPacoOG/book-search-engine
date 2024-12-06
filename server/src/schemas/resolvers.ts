c92ca076e81d17d8f5e18e15addfabf18b74abc8
Michael Fleming <mikahl171@gmail.com>
2024-12-06 17:17:49 -0600

    final push for working app. Will deploy now

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,89 +1,62 @@
-//taken directly from the module 18 activity 26 and modified to fit the needs of the project
+// taken from module 18, activity 26 and mofidifed
 
 import User from '../models/User.js';
-import Book from '../models/Book.js';
-import { signToken, AuthenticationError } from '../utils/auth.js';
-import { searchGoogleBooks, saveBook, removeBook } from '../utils/book.js';
-
-// Define types for the arguments
-interface AddUserArgs {
-  input: {
-    username: string;
-    email: string;
-    password: string;
-  };
-}
-
-interface LoginUserArgs {
-  email: string;
-  password: string;
-}
-
-interface UserArgs {
-  username: string;
-}
-
-interface BookArgs {
-  bookId: string;
-}
-
-interface SaveBookArgs {
-  input: {
-    bookId: string;
-    title: string;
-    authors: [string];
-  };
-}
+import { signToken } from '../services/auth.js';
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find().populate('savedBooks');
     },
-    user: async (_parent: any, { username }: UserArgs) => {
+    user: async (_parent: any, { username }: { username: string }) => {
       return User.findOne({ username }).populate('savedBooks');
     },
     me: async (_parent: any, _args: any, context: any) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate('savedBooks');
       }
-      throw new AuthenticationError('Could not authenticate user.');
-    },
-    searchBooks: async (_parent: any, { query }: { query: string }) => {
-      return searchGoogleBooks(query);
+      throw new Error('Could not authenticate user.');
     },
   },
   Mutation: {
-    addUser: async (_parent: any, { input }: AddUserArgs) => {
+    addUser: async (_parent: any, { input }: { input: { username: string; email: string; password: string } }) => {
       const user = await User.create({ ...input });
-      const token = signToken(user);
+      const token = signToken(user.username, user.password, user.email);
       return { token, user };
     },
-    login: async (_parent: any, { email, password }: LoginUserArgs) => {
+    login: async (_parent: any, { email, password }: { email: string; password: string }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('Could not authenticate user.');
+        throw new Error('Could not authenticate user.');
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw new AuthenticationError('Could not authenticate user.');
+        throw new Error('Could not authenticate user.');
       }
-      const token = signToken(user);
+      const token = signToken(user.username, user.password, user.email);
       return { token, user };
     },
-    saveBook: async (_parent: any, { input }: SaveBookArgs, context: any) => {
+    saveBook: async (_parent: any, { input }: { input: { bookId: string; title: string; authors: string[] } }, context: any) => {
       if (context.user) {
-        const updatedUser = await saveBook(context.user, input);
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        ).populate('savedBooks');
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new Error('You need to be logged in!');
     },
-    removeBook: async (_parent: any, { bookId }: BookArgs, context: any) => {
+    removeBook: async (_parent: any, { bookId }: { bookId: string }, context: any) => {
       if (context.user) {
-        const updatedUser = await removeBook(context.user, bookId);
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new Error('You need to be logged in!');
     },
   },
 };
