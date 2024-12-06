94bec1764902faa7c869be4b545d73188e748545
Michael Fleming <mikahl171@gmail.com>
2024-12-05 21:19:26 -0600

    startting to add code for apollo and graphql heavily dependent on activity 25/26

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,7 +1,8 @@
 // //taken directly from assignment 26
 
-// import { Thought, User } from '../models/index.js';
-// import { signToken, AuthenticationError } from '../utils/auth.js'; 
+import models from '../models/index.js';
+const User = models.User;
+import { signToken, AuthenticationError } from '../utils/auth.js';
 
 // // Define types for the arguments
 // interface AddUserArgs {
@@ -12,14 +13,14 @@
 //   }
 // }
 
-// interface LoginUserArgs {
-//   email: string;
-//   password: string;
-// }
+interface LoginUserArgs {
+  email: string;
+  password: string;
+}
 
-// interface UserArgs {
-//   username: string;
-// }
+interface UserArgs {
+  username: string;
+}
 
 // interface ThoughtArgs {
 //   thoughtId: string;
@@ -42,14 +43,14 @@
 //   commentId: string;
 // }
 
-const resolvers: unknown = {
+const resolvers = {
   Query: {
     // users: async () => {
     //   return User.find().populate('thoughts');
     // },
-    // user: async (_parent: any, { username }: UserArgs) => {
-    //   return User.findOne({ username }).populate('thoughts');
-    // },
+    user: async (_parent: any, { username }: UserArgs) => {
+      return User.findOne({ username });
+    },
     // thoughts: async () => {
     //   return await Thought.find().sort({ createdAt: -1 });
     // },
@@ -80,43 +81,30 @@ const resolvers: unknown = {
 //       return { token, user };
 //     },
     
-//     login: async (_parent: any, { email, password }: LoginUserArgs) => {
-//       // Find a user with the provided email
-//       const user = await User.findOne({ email });
+    login: async (_parent: any, { email, password }: LoginUserArgs) => {
+      // Find a user with the provided email
+      const user = await User.findOne({ email });
     
-//       // If no user is found, throw an AuthenticationError
-//       if (!user) {
-//         throw new AuthenticationError('Could not authenticate user.');
-//       }
+      // If no user is found, throw an AuthenticationError
+      if (!user) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
     
-//       // Check if the provided password is correct
-//       const correctPw = await user.isCorrectPassword(password);
+      // Check if the provided password is correct
+      const correctPw = await user.isCorrectPassword(password);
     
-//       // If the password is incorrect, throw an AuthenticationError
-//       if (!correctPw) {
-//         throw new AuthenticationError('Could not authenticate user.');
-//       }
+      // If the password is incorrect, throw an AuthenticationError
+      if (!correctPw) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
     
-//       // Sign a token with the user's information
-//       const token = signToken(user.username, user.email, user._id);
+      // Sign a token with the user's information
+      const token = signToken(user.username, user.email, user._id);
     
-//       // Return the token and the user
-//       return { token, user };
-//     },
-//     addThought: async (_parent: any, { input }: AddThoughtArgs, context: any) => {
-//       if (context.user) {
-//         const thought = await Thought.create({ ...input });
-
-//         await User.findOneAndUpdate(
-//           { _id: context.user._id },
-//           { $addToSet: { thoughts: thought._id } }
-//         );
-
-//         return thought;
-//       }
-//       throw AuthenticationError;
-//       ('You need to be logged in!');
-//     },
+      // Return the token and the user
+      return { token, user };
+    },
+  
 //     addComment: async (_parent: any, { thoughtId, commentText }: AddCommentArgs, context: any) => {
 //       if (context.user) {
 //         return Thought.findOneAndUpdate(
